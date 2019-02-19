b2e9ad7e143dc0c1af5406d168ca4e0ff4c03cc3
krzysztof-kucinski <47742087+krzysztof-kucinski@users.noreply.github.com>
2019-02-19 19:44:16 +0100

    Add files via upload

---
+++ SMALL-TALK.py
@@ -0,0 +1,16 @@
+print("WHAT IS YOUR NAME?")
+
+imie=input()
+
+print("NICE TO MEET YOU", imie)
+print()
+
+print("HOW OLD ARE YOU?")
+
+wiek=input()
+
+print("FROM WHICH PLATFORM YOU WILL GO TO HOGWARDS?")
+
+peron=input()
+
+print(imie,"You are",wiek,"years old. I do not think it makes sense to go to Hogwarts from platform",peron)
