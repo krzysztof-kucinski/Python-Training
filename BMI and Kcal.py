8182951601c255ad42bbe0c541a7962839f233a7
krzysztof-kucinski <47742087+krzysztof-kucinski@users.noreply.github.com>
2019-02-19 20:24:08 +0100

    Update BMI and Kcal.py

--- BMI and Kcal.py
+++ BMI and Kcal.py
@@ -1,20 +1,25 @@
 #BMI
-wzrost = 1.98
-waga = 105
-BMI = waga / (wzrost ** 2)
-#print("Twoje BMI wynosi:",BMI)
-print("Your BMI is: ", 105 / 1.98**2)
+print()
+wzrost = float(input("How tall are you?: "))
+print()
+waga = int(input("How much you weigh?: "))
+print()
 
+BMI = str(waga / (wzrost ** 2)*10000)
+#print("Twoje BMI wynosi:",BMI)
+print("Your BMI is: ", BMI)
+print()
+print("Now make sure that you eat well...")
+print()
+wiek = int(input("How old are you?: "))
+print()
 #Zapotrzebowanie kaloryczne'
-#PPM: 10 x masa ciała + 6.25 x wzrost w cm – 5 x wiek  + S'
+#PPM: 10 x masa ciała + 6.25 x wzrost w cm + 5 x wiek  + S'
 #współczynnik S: dla kobiet = -161, dla mężczyzn= +5'
 #Praca niefizyczna, mało aktywny tryb życia	1,4 - pomnożyć dzienne zapotrzebowanie'
-print()
-s= 5
-waga=105
-wzrost=198
-wiek=26
-PPM=10*waga+6.25*wzrost+5*25+s
-print("Your daily calories goal is:", PPM * 1.4,"kcal")
-print()
-print("So now you could go to the gym!")
+s=5
+
+PPM=((10 * waga + 6.25 * wzrost + 5 * 25 + s))
+
+print("So... Your daily calories goal is:", PPM * 1.4,"kcal")
+print("So now you could go to the gym! And no excuses!")
