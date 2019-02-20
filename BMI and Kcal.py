1edd0f526835f74b9802dc005818f8d1d83f3188
krzysztof-kucinski <47742087+krzysztof-kucinski@users.noreply.github.com>
2019-02-20 11:37:51 +0100

    Update BMI and Kcal.py

--- BMI and Kcal.py
+++ BMI and Kcal.py
@@ -2,16 +2,16 @@
 print()
 wzrost = float(input("How tall are you?: "))
 print()
-waga = int(input("How much you weigh?: "))
+waga = float(input("How much you weigh?: "))
 print()
 
-BMI = str(waga / (wzrost ** 2)*10000)
+BMI = float(waga / (wzrost ** 2)*10000)
 #print("Twoje BMI wynosi:",BMI)
-print("Your BMI is: ", BMI)
+print("Twoje BMI jest równe: ", round(BMI,2))
 print()
 print("Now make sure that you eat well...")
 print()
-wiek = int(input("How old are you?: "))
+wiek = str(input("How old are you?: "))
 print()
 #Zapotrzebowanie kaloryczne'
 #PPM: 10 x masa ciała + 6.25 x wzrost w cm + 5 x wiek  + S'
@@ -19,7 +19,7 @@ print()
 #Praca niefizyczna, mało aktywny tryb życia	1,4 - pomnożyć dzienne zapotrzebowanie'
 s=5
 
-PPM=((10 * waga + 6.25 * wzrost + 5 * 25 + s))
+PPM=float(((10 * waga + 6.25 * wzrost + 5 * 25 + s)))
 
-print("So... Your daily calories goal is:", PPM * 1.4,"kcal")
+print("So... Your daily calories goal is:", round(PPM * 1.4,2),"kcal")
 print("So now you could go to the gym! And no excuses!")
